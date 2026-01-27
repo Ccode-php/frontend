90bf8bf7b643f504c5ef641af6b02ea60a9321bd
Ccode-php <ccodephp@gmail.com>
2026-01-28 02:14:43 +0500

    kiying

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: '/api/auth', // backend URL
+  baseURL: 'http://dev.myapp.local/api', // backend URL
   //baseURL: import.meta.env.VITE_API_URL, // backend URL
 })
 
